3c073666e0b8c8401a658474760eeca84ea44441
Briana Eng <brianaeng@gmail.com>
2016-10-31 16:15:53 -0700

    Updated thoughts on pages added in comments

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -3,7 +3,11 @@ class HomepagesController < ApplicationController
     #Tentatively thinking to use this for the homepage, where the search bar sits
   end
 
+  def search_results
+    #Tentatively thinking to use this for search results
+  end
+
   def show
-    #Tentatively thinking to use this for the search results (or maybe they'll just repopulate on the index?)
+    #Tentatively thinking to use this for each recipe show
   end
 end
