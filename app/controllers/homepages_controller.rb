c321cdde00325ea3448b068065ea740cd2d31772
Briana Eng <brianaeng@gmail.com>
2016-10-31 16:05:06 -0700

    Added homepages controller and env file

---
+++ app/controllers/homepages_controller.rb
@@ -0,0 +1,9 @@
+class HomepagesController < ApplicationController
+  def index
+    #Tentatively thinking to use this for the homepage, where the search bar sits
+  end
+
+  def show
+    #Tentatively thinking to use this for the search results (or maybe they'll just repopulate on the index?)
+  end
+end
