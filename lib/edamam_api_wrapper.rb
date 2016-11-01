cd27b42775f68a73f62d8f0307ee653322031860
Briana Eng <brianaeng@gmail.com>
2016-11-01 08:37:34 -0700

    Added to results page and fixed wrapper.

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -2,11 +2,18 @@ class Edamam_Api_Wrapper
   ID = ENV["APP_ID"]
   KEY = ENV["APP_KEY"]
   BASE_URL = "https://api.edamam.com/search"
+  MAX_NUM_RESULTS = 20 #Keeping this as 20 for now since default is only 10, can increase later
+
 
   def self.find_recipes(search_term)
-    url = BASE_URL + "?q=#{search_term}" + "&app_id=#{ID}&app_key=#{KEY}"
+    #Compiled URL with constants and given search term
+    url = BASE_URL + "?q=#{search_term}" + "&app_id=#{ID}&app_key=#{KEY}" + "&to=#{MAX_NUM_RESULTS}"
 
+    #Using HTTParty to get URL response
     response = HTTParty.get(url)
+
+    #Will return an array of recipe hashes
+    return response["hits"]
   end
 
 end
