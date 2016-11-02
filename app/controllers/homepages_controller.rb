e6b75db549947f32b77c12ee0dc3b0233c5cd10a
Briana Eng <brianaeng@gmail.com>
2016-11-02 10:37:16 -0700

    Cleaning up controller for new attempt

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,7 +1,5 @@
 require "#{Rails.root}/lib/edamam_api_wrapper.rb"
 require "#{Rails.root}/lib/recipe.rb"
-require 'will_paginate/array'
-
 
 class HomepagesController < ApplicationController
 
@@ -9,30 +7,8 @@ class HomepagesController < ApplicationController
   end
 
   def search_results
-    @results = Edamam_Api_Wrapper.find_recipes(params[:search_term]).paginate(:page => params[:page], per_page: 10)
+    @results = Edamam_Api_Wrapper.find_recipes(params[:search_term])
 
-    # Attempt to save results via session but the hash was too big (even when JSON?) to store
-    # if session[:search_results] != nil
-    #   @pages = JSON.parse(session[:search_results])
-    #   @num_pages = @pages.length
-    # else
-    #   @results = Edamam_Api_Wrapper.find_recipes(params[:search_term])
-    #
-    #   @num_pages = @results.length/10
-    #   max_num = 10
-    #   page_num = 1
-    #
-    #   @pages = {}
-    #
-    #   @num_pages.times do
-    #     @page_results = @results[0..max_num]
-    #     @pages["#{page_num}"] = @page_results
-    #     max_num += 10
-    #     page_num += 1
-    #   end
-    #
-    #   session[:search_results] = @pages.to_json
-    # end
   end
 
   def show
@@ -42,9 +18,5 @@ class HomepagesController < ApplicationController
     @recipe_ingredients = params[:recipe_ingredients]
     @recipe_dietary_info = params[:recipe_dietary_info]
   end
-
-  private
-  def recipe_params
-    params.permit(:name, :user_id, :price, :quantity, :description, :picture, :active, categories_attributes: [:name], category_ids: [])
-  end
+  
 end
