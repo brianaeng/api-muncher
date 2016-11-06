1b0fba1cb1f0ce046f2c3702225649b0d1add1a8
Briana Eng <brianaeng@gmail.com>
2016-11-06 11:51:59 -0800

    Added coverage and a few additional tests

--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -4,9 +4,39 @@ require 'recipe'
 
 class EdamamApiWrapperTest < ActionController::TestCase
 
-  test 'all search results can be retrieved from Edamam' do
+  test 'all search results can be retrieved from Edamam with a search term' do
     VCR.use_cassette("all recipes") do
       all_recipes = EdamamApiWrapper.find_recipes("tomato")
+
+      assert all_recipes.is_a? Array
+      assert all_recipes.length > 0
+      assert all_recipes[0].is_a? Recipe
+    end
+  end
+
+  test 'all search results can be retrieved from Edamam with a health term' do
+    VCR.use_cassette("all recipes with health term") do
+      all_recipes = EdamamApiWrapper.find_recipes("tomato", ["peanut-free"])
+
+      assert all_recipes.is_a? Array
+      assert all_recipes.length > 0
+      assert all_recipes[0].is_a? Recipe
+    end
+  end
+
+  test 'all search results can be retrieved from Edamam with a diet term' do
+    VCR.use_cassette("all recipes with diet term") do
+      all_recipes = EdamamApiWrapper.find_recipes("tomato", nil, ["balanced"])
+
+      assert all_recipes.is_a? Array
+      assert all_recipes.length > 0
+      assert all_recipes[0].is_a? Recipe
+    end
+  end
+
+  test 'all search results can be retrieved from Edamam with a search, health, and diet term' do
+    VCR.use_cassette("all recipes with all terms") do
+      all_recipes = EdamamApiWrapper.find_recipes("tomato", ["peanut-free"], ["balanced"])
 
       assert all_recipes.is_a? Array
       assert all_recipes.length > 0
